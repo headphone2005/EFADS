913573edaee79b8ff45a8654656ee4f8a5ca6d6b
Chris G <59775715+headphone2005@users.noreply.github.com>
2024-04-06 18:12:49 -0400

    Add files via upload

--- get_Readings.ino.ino
+++ get_Readings.ino.ino
@@ -9,33 +9,40 @@ int dhtPin = 7;
 DHT22 dht(dhtPin);
 int smokeSensorVal; // Variable to store analog sensor value
 int smokeDigitalVal; // Variable to store digital sensor value
-
+float tempVal; // Variable to store temperature value
+float humidVal; // Variable to store humidity value
+int smokeDPin = 3; // The digital pin for the smoke sensor
+int LEDPin = 4; // The pin for LED
 
 void setup() {
   // put your setup code here, to run once:
   //Give dht and smoke sensor readings a place to be read.
   Serial.begin(9600);
-  pinMode(13, OUTPUT); //For LED indication
-  pinMode (3, INPUT); //For digital output from smoke sens
+  pinMode(13, OUTPUT); //For LED indication of the threshold LED
+  pinMode (smokeDPin, INPUT); //For digital output from smoke sens
+  pinMode(LEDPin, OUTPUT); // LED for when a threshold is reached
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  smokeSensorVal = analogRead(A3); //Get the CO2 ppm value
-  smokeDigitalVal = digitalRead(2); //Get digital value
+  smokeSensorVal = analogRead(A3); // Get the CO2 ppm value
+  smokeDigitalVal = digitalRead(2); // Get digital value
+  tempVal = dht.getTemperature(); // Get temperature read from dht
+  humidVal = dht.getHumidity(); // Get humidity % from dht
 
   Serial.print("Temperature = "); 
-  Serial.println(dht.getTemperature()); //Obtain the temperature read from the dht and output to serial monitor
+  Serial.print(tempVal); // Output temp to Serial monitor
+  Serial.println(" degrees Celcius");
   Serial.print("Humidity = ");
-  Serial.println(dht.getHumidity()); //Do the same but with the humidity
-  Serial.print("Smoke PPM = ");
-  Serial.println(smokeSensorVal); //Print the CO2 PPM
+  Serial.print(humidVal); // The same with humidity
+  Serial.println("%");
+  Serial.print("CO2 PPM = ");
+  Serial.println(smokeSensorVal); // The same with C02 PPM
   Serial.print("Digital Val: ");
-  Serial.println(smokeDigitalVal); //Print the digital val. 1 if within threshold. 0 if not.
-  delay(2000);
+  Serial.println(smokeDigitalVal); // The same with digital value of smoke sensor
 
-  // Testing gas concentration. 0 if out of bounds, 1 if within threshold. Turns on LED
+  // Testing gas concentration. 0 if out of bounds, 1 if within threshold. Turns on LED on sensor
   if (smokeDigitalVal == LOW)
    {
     digitalWrite(13, HIGH); // Turn on the LED
@@ -44,5 +51,24 @@ void loop() {
    {
     digitalWrite(13, LOW); // Turn off the LED
   }
+
+  // Changes the brightness depending on severity of fire danger
+  /*if (smokeSensorVal > 230 && tempVal > 23) {
+    analogWrite(LEDPin, 255);
+  }
+  else*/ if (smokeSensorVal > 230 || tempVal > 23) {
+    digitalWrite(LEDPin, HIGH);
+  }
+  else{
+    digitalWrite(LEDPin, LOW);
+  }
+  delay(2000);
+  /*
+  digitalWrite(LEDPin, HIGH);
+  delay(1000);
+  digitalWrite(LEDPin, 100);
+  delay(1000);
+  digitalWrite(LEDPin, LOW);
+  delay(500);*/
 
 }
